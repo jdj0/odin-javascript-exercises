8774dfa287c0077eee2c6c9ba5368d4222225279
Joshua Jones <joshua.davidjones92@gmail.com>
2023-05-16 14:28:08 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let bigString = "";
+    if (num < 0){
+        return "ERROR";
+    } else {
+    for (let i = 0; i < num; i++){
+        bigString += string;
+    }}
+    return bigString
 };
 
 // Do not edit below this line
