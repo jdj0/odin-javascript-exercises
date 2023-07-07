a61b7e36e5012f6d4ea0fb52981266ea0211872c
Joshua Jones <joshua.davidjones92@gmail.com>
2023-07-07 21:45:31 +0100

    Complete 09_palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let reg = /[\W_]/g;
+    let oldStr = str.toLowerCase().replace(reg, "");
+    let lowStr = str.toLowerCase().replace(reg, "");
+    let revStr = lowStr.split("").reverse().join("");
+    console.log(str)
+    console.log(lowStr)
+     if (revStr === oldStr) {
+      return true;
+    } else {
+      return false;
+    }
 };
 
 // Do not edit below this line
